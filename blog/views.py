02fb4fd234317dcdf0b1af89b3c39e22f75d8877
Jan Kusák <honzakusak@seznam.cz>
2024-07-09 15:59:23 +0200

    Modify views

--- blog/views.py
+++ blog/views.py
@@ -14,12 +14,19 @@ logger = logging.getLogger('django')
 
 
 class IndexView(generic.ListView):
+    """
+    A view that displays a list of articles based on search conditions.
+    """
+
     template_name: str = "blog/index.html"
     context_object_name: str = "articles"
     
     def get_queryset(self) -> QuerySet[Article]:
         """
-        Return articles according conditions.
+        Return articles according to the search conditions.
+
+        Returns:
+            QuerySet[Article]: The queryset of articles.
         """
         cache_key: str = 'multi'
         conditions = None
@@ -27,16 +34,16 @@ class IndexView(generic.ListView):
         query: str | None  = self.request.GET.get('q', None)
         tag: str | None  = self.request.GET.get('tag', None)
 
-        if query is not None and query:
-
+        if query:
+            # Clean and escape the query
             clean_query = escape(query)
             cache_key = f'{cache_key}-{clean_query}'
             post_query = SearchQuery(clean_query, search_type='phrase')
 
             conditions = Q(search=post_query)
             
-        if tag is not None and tag:
-
+        if tag:
+            # Clean and escape the tag
             clean_tag = escape(tag)
             cache_key = f'{cache_key}-{clean_tag}'
 
@@ -47,7 +54,7 @@ class IndexView(generic.ListView):
 
         articles: QuerySet[Article] | None = cache.get(cache_key)
 
-        if conditions is not None:
+        if conditions:
             if articles is None:
                 articles = Article.objects.annotate(search=SearchVector('title')).filter(conditions)[:3]
                 cache.set(cache_key, articles)
@@ -62,7 +69,10 @@ class IndexView(generic.ListView):
     
     def get_context_data(self, *args, **kwargs) -> Mapping[str, Any]:
         """
-        Return updated context with added Tags and previous query.
+        Return the updated context with added tags and previous query.
+
+        Returns:
+            Mapping[str, Any]: The updated context.
         """
         context = super().get_context_data(*args, **kwargs)
         cache_key: str = 'tags'
@@ -80,7 +90,7 @@ class IndexView(generic.ListView):
         context['prev_query'] = escape(prev_query) if prev_query is not None else prev_query
         context['prev_tag'] = escape(prev_tag) if prev_tag is not None else prev_tag
         
-        return context 
+        return context
 
 
 class ArticlesView(generic.ListView):
@@ -90,7 +100,10 @@ class ArticlesView(generic.ListView):
     
     def get_queryset(self) -> QuerySet[Article]:
         """
-        Return all articles.
+        This method retrieves all articles from the database or from cache if they exist there. 
+
+        Returns:
+            QuerySet[Article]: All articles in the database.
         """
         cache_key: str = 'articles'
 
